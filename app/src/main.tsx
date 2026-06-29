9a60fbac8a6b0e90a00b2bfddc35a99a31a85b27
yassine0malal <malalyassin6@gmail.com>
2026-06-29 06:32:03 +0100

    (fix): a problem in the CI/CD pipline

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/aslmad-tamazight">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>,
