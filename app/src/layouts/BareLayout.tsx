a701a86608c64a07b5e2471ea5caf0ab69b98a42
yassine0malal <malalyassin6@gmail.com>
2026-06-29 08:41:02 +0100

    (fix): abig problem of files

--- app/src/layouts/BareLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router'
-
-export default function BareLayout() {
-  return (
-    <div className="min-h-screen w-full" style={{ background: 'var(--clay-canvas)' }}>
-      <Outlet />
-    </div>
-  )
-}
